8858b6f1e1c91250c692ede21461f0890056fed1
bill geng <billgeng@yahoo.com>
2021-08-17 13:07:37 -0400

    add app.js

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-var commandLinerArgs = process.argv;
-console.log(commandLinerArgs);
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i =0;i< profileDataArr.length;i++) {
+        console.log(profileDataArr[i]);
+    }
+     
+    console.log('======================');
+
+    profileDataArr.forEach((profileItem) =>{
+        console.log(profileItem)
+    });
+
+    // same like : profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData (profileDataArgs);
